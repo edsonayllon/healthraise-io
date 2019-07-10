940bf010b7795f18f8832c7d8ebd3b0eb04260c9
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-10 04:33:51 +0000

    updated contracts

--- blockchain/contracts/HealthraiseRoot.sol
+++ blockchain/contracts/HealthraiseRoot.sol
@@ -17,14 +17,30 @@ contract HealthraiseRoot is Ownable {
     Account[] public accounts;
 
     mapping (address => uint) accountCount;
+    mapping (address => uint) accountId;
     mapping (uint => address) public accountToOwner;
 
     function createAccount(string _name) public {
         require(accountCount[msg.sender] == 0);
         uint id = accounts.push(Account(_name, id, 0, 0, 0)) - 1;
         accountCount[msg.sender]++;
+        accountId[msg.sender] = id;
         accountToOwner[id] = msg.sender;
         emit AccountAdded(id, _name, msg.sender);
     }
 
+    function sponsor(address _to, uint amount) public {
+        uint id = accountId[_to];
+        accounts[id].raised = accounts[id].raised + amount;
+    }
+
+    // do something to make sure each person can only vote on one entity once
+    function rate(address _to, bool _positive) public {
+        uint id = accountId[_to];
+        if (_positive) {
+            accounts[id].upvote++;
+        } else {
+            accounts[id].downvote++;
+        }
+    }
 }
